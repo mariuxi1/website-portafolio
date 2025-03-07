3e91a556305c0ab192b2ae52f3ba313d274b6c4a
mariuxi1 <mryg28@outlook.com>
2025-03-06 19:50:34 -0500

    added contact me box

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-function addRecommendation() {
-  // Get the message of the new recommendation
-  let recommendation = document.getElementById("new_recommendation");
-  // If the user has left a recommendation, display a pop-up
-  if (recommendation.value != null && recommendation.value.trim() != "") {
-    console.log("New recommendation added");
+function sendMessage() {
+  // Get the message
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("new_message").value;
+
+  if(!subject)
+  {
+    console.log("Subject line is missing.");   
+    alert("Please enter a subject.");
+  }
+  else if(!message)
+  {
+    console.log("Message is missing.");   
+    alert("Please enter a message.");
+  }
+  else
+  {
+    const email = "mariuxiy@gmail.com"
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+    // Open default email client
+    window.location.href = mailtoLink;
+    console.log("Message app request sent.");
+        
     //Call showPopup here
     showPopup(true);
 
-    // Create a new 'recommendation' element and set it's value to the user's message
-    var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
-    // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
-    // Reset the value of the textarea
-    recommendation.value = "";
+    message.value = "";
   }
 }
 
